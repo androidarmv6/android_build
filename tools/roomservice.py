acb6063ef653f5e2436e119c3ebe6f21c812365e
AndroidMeda <hephappy@gmail.com>
2013-02-27 13:31:39 +0200

    Revert "build: local_manifest.xml deprecated, use local_manifests"
    This reverts commit 3084c0097233a191cbaaba34a0e014d6313be341.
    
    Conflicts:
    
    	tools/roomservice.py
    
    Change-Id: I0dbb134c9a37e7292e8ce28229c96023c2c51f98

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -60,9 +60,6 @@ while not depsonly:
         repositories.append(res)
     page = page + 1
 
-local_manifests = r'.repo/local_manifests'
-if not os.path.exists(local_manifests): os.makedirs(local_manifests)
-
 def exists_in_tree(lm, repository):
     for child in lm.getchildren():
         if child.attrib['name'].endswith(repository):
@@ -87,7 +84,7 @@ def indent(elem, level=0):
 
 def get_from_manifest(devicename):
     try:
-        lm = ElementTree.parse(".repo/local_manifests/roomservice.xml")
+        lm = ElementTree.parse(".repo/local_manifest.xml")
         lm = lm.getroot()
     except:
         lm = ElementTree.Element("manifest")
@@ -111,7 +108,7 @@ def get_from_manifest(devicename):
 
 def is_in_manifest(projectname):
     try:
-        lm = ElementTree.parse(".repo/local_manifests/roomservice.xml")
+        lm = ElementTree.parse(".repo/local_manifest.xml")
         lm = lm.getroot()
     except:
         lm = ElementTree.Element("manifest")
@@ -124,7 +121,7 @@ def is_in_manifest(projectname):
 
 def add_to_manifest(repositories):
     try:
-        lm = ElementTree.parse(".repo/local_manifests/roomservice.xml")
+        lm = ElementTree.parse(".repo/local_manifest.xml")
         lm = lm.getroot()
     except:
         lm = ElementTree.Element("manifest")
@@ -149,7 +146,7 @@ def add_to_manifest(repositories):
     raw_xml = ElementTree.tostring(lm)
     raw_xml = '<?xml version="1.0" encoding="UTF-8"?>\n' + raw_xml
 
-    f = open('.repo/local_manifests/roomservice.xml', 'w')
+    f = open('.repo/local_manifest.xml', 'w')
     f.write(raw_xml)
     f.close()
 
