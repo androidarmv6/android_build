2dad106c622c6fdbfe754a3fa8acd5aedb609a4d
AndroidMeda <hephappy@gmail.com>
2014-07-24 22:30:08 +0200

    Revert "Remove Calendar & CalendarProvider"
    This reverts commit 61d7e2974f7c07801d913223c8cc4495fec141ec.
    
    Change-Id: If5d040e85625593f93e807a147909727d1c64951

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -23,6 +23,7 @@ PRODUCT_PACKAGES := \
     DeskClock \
     Bluetooth \
     Calculator \
+    Calendar \
     Camera2 \
     CertInstaller \
     FusedLocation \
@@ -38,6 +39,7 @@ PRODUCT_PACKAGES := \
     Settings \
     SystemUI \
     TeleService \
+    CalendarProvider \
     bluetooth-health \
     hostapd \
     wpa_supplicant.conf \
@@ -46,10 +48,6 @@ PRODUCT_PACKAGES := \
 ifneq ($(MINI_GAPPS),true)
 PRODUCT_PACKAGES += \
     QuickSearchBox
-else
-PRODUCT_PACKAGES += \
-    Calendar \
-    CalendarProvider
 endif
 
 PRODUCT_PACKAGES += \
