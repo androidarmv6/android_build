d3999fd63f2ebec56fc4a75c18298283720efa4f
Marco Milanese <marcomilanese7@gmail.com>
2014-07-28 21:55:42 +0200

    build: armv6-vfp.mk set arch to armv6k
    arm1136 has some variants:
    r1p0            -> armv6
    r1p1 or higher  -> armv6k
    
    msm7x27's core is r1p1 or higher so it supports full armv6k
    instruction set
    
    In armv6-vfp.mk we set mcpu flag to arm1136jf-s, but gcc doesn't
    know that our core is r1p1 or higher so it thinks[1] the target arch
    to be armv6
    
    Unfortunately there is no way to set arm1136 variant r1p1 or higher
    through the mcpu flag, thus we should use the march flag and set it
    to armv6k
    
    [1]
    ldrexb (load exclusive byte) is a armv6k+ instruction:
    
    mm7@userver:~$ echo "ldrexb r0, [r1]" > test.s
    mm7@userver:~$ ./arm-eabi-as test.s -mcpu=arm1136jf-s
    test.s: Assembler messages:
    test.s:1: Error: selected processor does not support `ldrexb r0,[r1]'
    mm7@userver:~$ ./arm-eabi-as test.s -march=armv6
    test.s: Assembler messages:
    test.s:1: Error: selected processor does not support `ldrexb r0,[r1]'
    mm7@userver:~$ ./arm-eabi-as test.s -march=armv6k
    mm7@userver:~$
    
    Change-Id: I1e47969b2f71a19eafdd814581357499f853cb8b
    Signed-off-by: Marco Milanese <marcomilanese7@gmail.com>

--- core/combo/arch/arm/armv6-vfp.mk
+++ core/combo/arch/arm/armv6-vfp.mk
@@ -15,19 +15,19 @@ endif
 ifeq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
 TARGET_ARCH_VARIANT_FPU         := vfp
 endif
-ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
-TARGET_ARCH_VARIANT_CPU         := arm1136jf-s
-endif
 
 # Note: Hard coding the 'tune' value here is probably not ideal,
 # and a better solution should be found in the future.
 #
 arch_variant_cflags := \
-    -mcpu=$(TARGET_ARCH_VARIANT_CPU) \
+    -march=armv6k \
     -mfloat-abi=softfp \
     -mfpu=$(TARGET_ARCH_VARIANT_FPU) \
     -D__ARM_ARCH_5__ \
     -D__ARM_ARCH_5T__ \
     -D__ARM_ARCH_5E__ \
-    -D__ARM_ARCH_5TE__
+    -D__ARM_ARCH_5TE__ \
+    -D__ARM_ARCH_6__ \
+    -D__ARM_ARCH_6J__ \
+    -D__ARM_ARCH_6K__
 
