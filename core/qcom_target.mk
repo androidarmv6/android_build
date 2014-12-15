4c6caa30d0d32a5a55687337e6be35f15a680c05
Marco Milanese <marcomilanese7@gmail.com>
2014-12-15 21:05:26 +0100

    build: don't enable QCOM_BSP unconditionally
    Change-Id: I2ca12243af5493c24e4b48ba78d2a12cb3b1b3c8

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -8,9 +8,11 @@ endef
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
     qcom_flags := -DQCOM_HARDWARE
-    qcom_flags += -DQCOM_BSP
 
-    TARGET_USES_QCOM_BSP := true
+    ifeq ($(TARGET_USES_QCOM_BSP),true)
+        qcom_flags += -DQCOM_BSP
+    endif
+
     TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
     # Enable DirectTrack for legacy targets
